636f2e5482a5b77ee7a53ef85fff506fa0a7ff91
Xone130 <pereirinhafilipe@gmail.com>
2024-10-27 18:45:55 +0000

    constantes

--- Main.java
+++ Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Main{
 
-    public static void main(String[] args) {
-        //main
+    //CONSTANTS
+    public static final String INSERT_LINE = "IL"; //Inserção de linha
+    public static final String REMOVE_LINE = "RL"; //Remoção de linha
+    public static final String CHECK_LINE = "CL"; //Consulta das estações de uma linha
+    public static final String CHECK_STATION = "CE"; //Consulta das linhas de uma estação
+    public static final String ADD_TIMETABLE = "IH"; //inserção de horário
+    public static final String REMOVE_TIMETABLE = "RH"; //Remoção de horário
+    public static final String CHECK_LINE_TIMETABLES = "CH"; //Consulta dos horários de uma linha
+    public static final String STATION_TRAINS = "LC"; //Comboios por estação
+    public static final String BEST_COURSE = "MH"; //Melhor Horário
+    public static final String QUIT = "TA"; //Terminar aplicação
 
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        
+        //command interpreter
+
+
+
+        in.close();
     }
 }
 
